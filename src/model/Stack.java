63cf423606748669cd0846ed26394f884c451122
ZabZarab <davzab2006@gmail.com>
2022-11-11 16:18:07 +0100

    stack ist fertig

--- src/model/Stack.java
+++ src/model/Stack.java
@@ -1,5 +1,58 @@
 package model;
 
 //TODO 01: Umsetzung der Klasse Stack gemäß der Dokumentations des Landes.
-public class Stack {
+public class Stack<ContentType> {
+
+    private class Node{
+        private Node next;
+        private ContentType content;
+
+        public Node(ContentType content){
+            this.content = content;
+            this.next = null;
+        }
+
+        public Node getNext(){
+            return next;
+        }
+
+        public void setNext(Node next){
+            this.next = next;
+        }
+        public ContentType getContent(){
+            return content;
+        }
+    }
+
+    private Node top;
+
+    public boolean isEmpty(){
+        if(top == null){
+            return true;
+        }
+        return false;
+    }
+
+    public void push(ContentType pContent){
+        Node n = new Node(pContent);
+        if(pContent != null){
+            if (!isEmpty()) {
+                n.setNext(top);
+            }
+            top = n;
+        }
+    }
+
+    public void pop(){
+        if(!isEmpty()){
+            top = top.getNext();
+        }
+    }
+
+    public ContentType top() {
+        if (!isEmpty()) return top.getContent();
+        return null;
+    }
 }
+
+
